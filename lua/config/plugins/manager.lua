fe4d2cb759b9d58054bf983a2d00844dd12b9719
Theis <theisc@pm.me>
2022-03-01 07:55:59 +0100

    renaming

--- lua/config/plugins/manager.lua
+++ lua/config/plugins/manager.lua
@@ -15,11 +15,11 @@ if fn.empty(fn.glob(install_path)) > 0 then
   vim.cmd [[packadd packer.nvim]]
 end
 
--- Autocommand that reloads neovim whenever you save the packer.lua file
+-- Autocommand that reloads neovim whenever you save the manager.lua file
 vim.cmd [[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost packer.lua source <afile> | PackerSync
+    autocmd BufWritePost manager.lua source <afile> | PackerSync
   augroup end
 ]]
 
