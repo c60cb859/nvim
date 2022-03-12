0042d38e32a3a15582d5543fc1f73b2349cf6b4a
Theis <theisc@pm.me>
2022-03-12 12:53:45 +0100

    update

--- lua/config/lsp/lspInstaller.lua
+++ lua/config/lsp/lspInstaller.lua
@@ -14,7 +14,7 @@ local servers = {
 for _, name in pairs(servers) do
   local server_is_found, server = lsp_installer.get_server(name)
   if server_is_found and not server:is_installed() then
-    print("Installing " .. name)
+    vim.notify("Installing " .. name)
     server:install()
   end
 end
