8d121e40b7c16f5db6f65a2ede46e17f5beb5193
Theis Christenesn <theisc@protonmail.com>
2022-03-26 09:11:50 +0100

    give list of treesitter parsers

--- lua/config/plugins/treeSitter.lua
+++ lua/config/plugins/treeSitter.lua
@@ -3,8 +3,53 @@ if not status_ok then
   return
 end
 
+local parsers = {
+  'bash',
+  'bibtex',
+  'c',
+  'cmake',
+  'comment',
+  'cpp',
+  'css',
+  'cuda',
+  'devicetree',
+  'dockerfile',
+  'dot',
+  'glsl',
+  'hjson',
+  'html',
+  'http',
+  'javascript',
+  'jsdoc',
+  'json',
+  'json5',
+  'JSON with comments',
+  'latex',
+  'llvm',
+  'lua',
+  'make',
+  'markdown',
+  'ninja',
+  'perl',
+  'pioasm',
+  'python',
+  'Tree-sitter query language',
+  'rasi',
+  'regex',
+  'rst',
+  'rust',
+  'scss',
+  'todotxt',
+  'toml',
+  'tsx',
+  'typescript',
+  'verilog',
+  'vim',
+  'yaml',
+},
+
 configs.setup {
-  ensure_installed = "maintained", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ensure_installed = parsers, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
   autopairs = {
