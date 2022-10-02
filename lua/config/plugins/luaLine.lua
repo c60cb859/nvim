de84df010bf9a5aaa200ff7400dcd22a4fa430ae
Theis Christenesn <theisc@protonmail.com>
2022-10-02 11:48:14 +0200

    add global status and winbar

--- lua/config/plugins/luaLine.lua
+++ lua/config/plugins/luaLine.lua
@@ -2,11 +2,11 @@ local filename = {
   'filename',
   file_status = true,      -- Displays file status (readonly status, modified status)
   path = 1,                -- 0: Just the filename
-                           -- 1: Relative path
-                           -- 2: Absolute path
+  -- 1: Relative path
+  -- 2: Absolute path
 
   shorting_target = 40,    -- Shortens path to leave 40 spaces in the window
-                           -- for other components. (terrible name, any suggestions?)
+  -- for other components. (terrible name, any suggestions?)
   symbols = {
     modified = '[+]',      -- Text to show when the file is modified.
     readonly = '[-]',      -- Text to show when the file is non-modifiable or readonly.
@@ -22,6 +22,7 @@ require('lualine').setup {
     section_separators = { left = '', right = ''},
     disabled_filetypes = {},
     always_divide_middle = true,
+    globalstatus = true,
   },
   sections = {
     lualine_a = {'mode'},
