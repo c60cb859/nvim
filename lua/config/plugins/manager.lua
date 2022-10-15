ee4b544823b3ac6180b926856d84908238c48486
Theis <theisc@pm.me>
2022-10-15 14:41:02 +0200

    update

--- lua/config/plugins/manager.lua
+++ lua/config/plugins/manager.lua
@@ -80,6 +80,7 @@ return packer.startup(function(use)
     use("neovim/nvim-lspconfig")
     use("williamboman/mason.nvim")
     use("williamboman/mason-lspconfig.nvim")
+    use("folke/trouble.nvim")
 
     -- DAP
     --use("mfussenegger/nvim-dap")
