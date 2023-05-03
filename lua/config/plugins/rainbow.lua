6c262ebce6c0d153b72898fc4f5b52551a072781
Theis Christenesn <theisc@protonmail.com>
2023-05-03 05:39:52 +0200

    refactor(Rainbow): Update config for rainbow

--- lua/config/plugins/rainbow.lua
+++ lua/config/plugins/rainbow.lua
@@ -1,14 +1,22 @@
-local status_ok, rainbow = pcall(require, "nvim-treesitter.configs")
-if not status_ok then
-	vim.notify("Rainbow not found!")
+local ts_config_ok, ts_config = pcall(require, "nvim-treesitter.configs")
+if not ts_config_ok then
+	vim.notify("Treesitter configs not found!")
 	return
 end
 
-rainbow.setup({
+local rainbow_ok, rainbow = pcall(require, "ts-rainbow")
+if not rainbow_ok then
+	vim.notify("ts-rainbow not found")
+	return
+end
+
+ts_config.setup({
 	rainbow = {
 		enable = true,
 		query = "rainbow-parens",
-		strategy = require("ts-rainbow").strategy.global,
+		strategy = {
+			rainbow.strategy["global"],
+		},
 		hlgroups = {
 			"TSRainbowBlue",
 			"TSRainbowViolet",
