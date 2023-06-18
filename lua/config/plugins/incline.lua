217dd8d4dc3e63aa3f0a366a200e5a792bb70be2
Theis Christenesn <theisc@protonmail.com>
2023-06-18 10:09:42 +0200

    Update nerdfont symbols

--- lua/config/plugins/incline.lua
+++ lua/config/plugins/incline.lua
@@ -6,9 +6,9 @@ end
 
 local render = function(props)
 	local bufname = vim.api.nvim_buf_get_name(props.buf)
-	local res = bufname ~= "" and vim.fn.fnamemodify(bufname, ":.t") or " [No Name]"
+	local res = bufname ~= "" and vim.fn.fnamemodify(bufname, ":.t") or "󰈤 [No Name]"
 	if vim.api.nvim_buf_get_option(props.buf, "modified") then
-		res = res .. " "
+		res = res .. " 󰏫"
 	end
 	return res
 end
