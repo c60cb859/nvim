061400137ac94d812397b93476e13e7a79c18dfa
Theis <theisc@pm.me>
2025-07-30 23:09:49 +0200

    Add multigrep with treesitter

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -4,23 +4,23 @@ return {
     build = ":TSUpdate",
     branch = "main",
     config = function()
-      require("nvim-treesitter").install {"c", "cpp", "lua", "rust", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
-      require'nvim-treesitter'.setup {
-	auto_install = false,
-	highlight = {
-	  enable = true,
+      require("nvim-treesitter").install { "c", "cpp", "lua", "rust", "python", "vim", "vimdoc", "query", "markdown", "markdown_inline" }
+      require 'nvim-treesitter'.setup {
+        auto_install = false,
+        highlight = {
+          enable = true,
 
-	  disable = function(lang, buf)
-	      local max_filesize = 100 * 1024 -- 100 KB
-	      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-	      if ok and stats and stats.size > max_filesize then
-		  return true
-	      end
-	  end,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
 
-	  additional_vim_regex_highlighting = false,
-	},
-       }
+          additional_vim_regex_highlighting = false,
+        },
+      }
     end,
   }
 }
