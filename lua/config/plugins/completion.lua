fc492cc2c0c5197253f54c89a7848c6583e41098
Theis <theisc@pm.me>
2025-08-01 06:41:44 +0200

    Add completion

---
+++ lua/config/plugins/completion.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    'saghen/blink.cmp',
+    enabled = true,
+    dependencies = { 'rafamadriz/friendly-snippets' },
+
+    version = '1.*',
+
+    ---@module 'blink.cmp'
+    ---@type blink.cmp.Config
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<C-p>'] = { 'show', 'select_prev', 'fallback_to_mappings' },
+        ['<C-n>'] = { 'show', 'select_next', 'fallback_to_mappings' },
+      },
+
+      appearance = {
+        nerd_font_variant = 'mono'
+      },
+      signature = { enabled = true },
+
+      completion = {
+        documentation = { auto_show = false },
+        trigger = { show_on_keyword = false },
+      },
+
+      sources = {
+        default = { 'lsp', 'buffer', 'path', 'snippets' },
+        providers = {
+          lsp = { fallbacks = {} }
+        }
+      },
+
+      fuzzy = { implementation = "prefer_rust_with_warning" }
+    },
+    opts_extend = { "sources.default" }
+  }
+}
